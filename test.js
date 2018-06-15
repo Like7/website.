78776001ee18d7b596e39333dec585fc877d1fd5
Lik7 <22848484+Like7@users.noreply.github.com>
2018-06-15 20:32:19 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 function test() {
-   var face = document.getElementById("icons");
+   var face = document.getElementById("icons1");
    face.style.opacity = "0";
-   var poo = document.getElementById("circ");
+   var poo = document.getElementById("circ1");
    poo.style.animationPlayState="paused";
 }
