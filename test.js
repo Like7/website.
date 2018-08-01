83556dd4bb929cb9122dad3536d36c3a329d51ee
Lik7 <22848484+Like7@users.noreply.github.com>
2018-08-01 12:19:00 +1000

    Update test.js

--- test.js
+++ test.js
@@ -2,8 +2,6 @@ function test() {
    document.getElementById("circ1").style.animationPlayState="paused";
 }
 
-function colr() {
-   var t = document.getElementsClassName("icon");
-   t[0].style.opacity = "0";
-   alert("test")
+function opa() {
+   document.getElementbyId("point", "click").style.opacity="0";
 }
