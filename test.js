fc8083f244d0f4a7122eeb519ee4c8d049808816
Lik7 <22848484+Like7@users.noreply.github.com>
2018-08-06 14:03:58 +1000

    Update test.js

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 function test() {
    document.getElementById("circ1").style.animationPlayState="paused";
+   document.getElementById("circ1").style.transform="scale(1)";
 }
 
 function opa() {
    document.getElementById("ico").style.opacity="0";
 }
+
